d10b669e65f5327c8641989477ebc192bdc06c3b
vanichols <vnichols@iastate.edu>
2019-04-11 13:48:53 -0500

    Gina plays with data

---
+++ data-raw/play_with_data.R
@@ -0,0 +1,41 @@
+all_sals
+sals18
+names(sals18)
+
+library(tidyverse)
+dat <- as_tibble(sals18)
+
+dat %>%
+  filter(department == "AGRONOMY") %>%
+  filter(grepl('PROF', position)) %>%
+  ggplot(aes(gender, total_salary_paid)) +
+  geom_jitter(aes(color = position), size = 3, width = 0.2) +
+  facet_grid(~department)
+
+
+dat %>%
+  #filter(department == "AGRONOMY") %>%
+  filter(grepl('PROF', position),
+         gender %in% c('M', 'F')) %>%
+  group_by(organization) %>%
+  mutate(n = n()) %>%
+  filter(n > 2) %>%
+  ggplot(aes(gender, total_salary_paid)) +
+  geom_jitter(aes(color = position), size = 3, width = 0.2) +
+  stat_summary(fun.y = mean, geom = "point") +
+  guides(color = F) +
+  facet_wrap(~organization)
+
+
+dat %>%
+  #filter(department == "AGRONOMY") %>%
+  filter(grepl('ASST PROF', position),
+         gender %in% c('M', 'F')) %>%
+  group_by(organization) %>%
+  mutate(n = n()) %>%
+  filter(n > 2) %>%
+  ggplot(aes(gender, total_salary_paid)) +
+  geom_jitter(aes(color = position), size = 3, width = 0.2) +
+  stat_summary(fun.y = mean, geom = "point") +
+#  guides(color = F) +
+  facet_wrap(~organization)
