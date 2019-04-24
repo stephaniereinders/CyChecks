5503b034b7ab696e3e186a85c99e4a175a2b26c2
Lydia English <lydia.english@gmail.com>
2019-04-24 11:26:31 -0500

    Updated anonymize - documented things

---
+++ R/sals18.R
@@ -0,0 +1,22 @@
+#' Salary information for Iowa State Univeristy employees in 2018
+#'
+#' A dataset containing the salary information, position, and gender of over
+#' 9,000 employees at Iowa State University
+#'
+#' @format A dataframe with 9,989 rows and 10 variables
+#' \describe{
+#' \item{base_salary_date}{the date base salary was computed}
+#' \item{fiscal_year}{year (July 1 - June 30) salary data is associated with}
+#' \item{gender}
+#' \item{name}{name of employee}
+#' \item{place_of_residence}{county or city whereby employee resides}
+#' \item{position}{position within the university}
+#' \item{total_salary_paid}{total amount (in dollars) of salary for that fiscal year}
+#' \item{travel_subsistence}{amount (in dollars) of travel expenditures reimbursed during the fiscal year}
+#' \item{organization}{larger umbrella organization under which the employee works}
+#' \item{department}{home department on campus}
+#' }
+#'
+#'
+#' @source\url{https://data.iowa.gov/State-Finances/State-of-Iowa-Salary-Book/s3p7-wy6w}
+"sals18"
