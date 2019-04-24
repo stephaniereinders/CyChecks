aa5157ba71cd0b44405c49523b55b30fd4cf95ac
Khaledian <ykh@iastate.edu>
2019-04-24 16:55:17 -0500

    moved test

---
+++ tests/testthat/test_anon_fun.r
@@ -0,0 +1,16 @@
+######### Testing anonymize function ############
+
+
+library(testthat)
+context("test_anonymize_function")
+
+test_that("Anonymize function is working", {
+  expect_true(is.character(cols_to_anon)) #
+  expect_true(is.data.frame(df)) #
+  expect_true(is.function(anonymize)) #
+  expect_error(is.string(algo)) # algorithm is a mix of numbers and letters
+  show_failure(df$travel_subsistence) # The column has values.
+  expect_true(is.numeric(df$total_salary_paid)) #
+  expect_true(is.factor(df$gender)) # gender is factor
+})
+
