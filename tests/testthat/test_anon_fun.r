d6508b7840043a72801f0bc1cb1baa8924d747da
Khaledian <ykh@iastate.edu>
2019-04-24 18:25:38 -0500

    updated2

--- tests/testthat/test_anon_fun.r
+++ tests/testthat/test_anon_fun.r
@@ -8,7 +8,3 @@ test_that("Anonymize function is working", {
   expect_error(anonymize(data.frame(name = letters), cols_to_anon = "name", algo = "crc32")) #--test col name is appropriate
 })
 
-
-head(test)
-
-expect_error(anonymize(sals_dept, cols_to_anon = 1)) #--test col name is character
