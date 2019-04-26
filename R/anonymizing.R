a10b243e4a45baefeff7daf4e7e3194dd0825033
vanichols <vnichols@iastate.edu>
2019-04-26 10:10:35 -0500

    Simplify anon function test (Gina)

--- R/anonymizing.R
+++ R/anonymizing.R
@@ -12,14 +12,19 @@
 #' anonymize(DF)
 
 anonymize <- function(df, cols_to_anon = "name", algo = "crc32"){
-  assertthat::see_if(is.character(cols_to_anon), msg = "The selected columns are not character!")
+
+  assertthat::see_if(is.character(cols_to_anon), msg = "The selected columns are not characters!")
   assertthat::see_if(cols_to_anon %in% names(df), msg = "The selected column isn't in the dataframe")
-  assertthat::assert_that(is.data.frame(df), msg = "df is not a dataframe!")
-  assertthat::assert_that(is.character(algo), msg = "algo is not string")
+  assertthat::assert_that(is.data.frame(df), msg = "df is not a dataframe!",
+                          is.character(algo), msg = "algo is not a character string")
+
   to_anon <- dplyr::select(df, cols_to_anon)
+
   ids <- unname(apply(to_anon, 1, digest::digest, algo = algo))
+
   df2 <- df %>%
     dplyr::mutate(id = ids)
+
   return(df2)
 }
 
--- tests/testthat/test_anon_fun.r
+++ tests/testthat/test_anon_fun.r
@@ -4,16 +4,7 @@ context("test_anonymize_function")
 
 
 test_that("Anonymize function is working", {
-  library(testthat)
-  cols_to_anon <- "example"
-  expect_true(is.character(cols_to_anon)) ##
-  expect_true(is.data.frame(df)) #
-  expect_true(is.function(anonymize)) #
-  expect_error(is.string(algo)) # algorithm is a mix of numbers and letters
-  show_failure(df$travel_subsistence) # The column has values.
-  expect_true(is.numeric(df$total_salary_paid)) #
-  expect_true(is.factor(df$gender)) # gender is factor
-  expect_error(anonymize(data.frame(name = letters), cols_to_anon = "name", algo = "crc32")) #--test col name is appropriate
+  expect_error(anonymize(sals18, "XX")) #--test col name is appropriate
 
 })
 
