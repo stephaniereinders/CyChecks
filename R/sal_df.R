1c3d754f2b61002e36732e6eba1a697d57d576e5
Lydia English <lydia.english@gmail.com>
2019-04-26 11:51:38 -0500

    moved shiny app to inst & created function to read it (LE)

--- R/sal_df.R
+++ R/sal_df.R
@@ -9,21 +9,23 @@
 #' @param token An API token. Only necessary for large amounts of datascraping. Generated from this \href{"https://dev.socrata.com/foundry/data.iowa.gov/s3p7-wy6w"}{website}
 #' @return A dataframe with salary information, position, and date for Iowa State University employees
 #'
-#' @importFrom dplyr mutate
-#' @importFrom dplyr select
+#' @importFrom dplyr mutate select
 #' @importFrom lubridate ymd_hms
-#' @importFrom checkmate assertNumber
-#' @importFrom checkmate assertTibble
+#' @importFrom checkmate assertNumber assertTibble assertDataFrame
 #' @importFrom tibble as_tibble
-#' @importFrom checkmate assertDataFrame
 #' @importFrom lubridate ymd_hms
 #' @importFrom forcats as_factor
 #' @importFrom stringr str_trim
 #' @importFrom jsonlite fromJSON
 #' @details An API (or APP) token isn't necessary for scraping data, but it will help speed up the data grabbing process and will allow users to get nearly unlimited data.
-#'
-#' @export
 #' @author Lydia English
+#' @examples
+#' \dontrun{
+#' sal_df()
+#' x <- sal_df(limit = 10, fiscal_year = 2015)
+#' }
+#' @export
+#'
 
 
 sal_df<- function(limit= 1000, offset = 0, fiscal_year = 2007, token = NULL){
