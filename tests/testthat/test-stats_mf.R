362488c8e62f7092e5c0717b479f80f56dfea0aa
Lydia English <lydia.english@gmail.com>
2019-05-02 10:16:48 -0500

    increased stats_mf coverage, tried to fix a vignette issue (LE)

--- tests/testthat/test-stats_mf.R
+++ tests/testthat/test-stats_mf.R
@@ -1,15 +1,18 @@
 context("test-stats_mf")
 
-
 test_that(
   "stats_mf is working", {
-    library(checkmate)
-    expect_error(stats_mf(data = select(sals_dept, -total_salary_paid))) # no total_salary_paid column
+  library(checkmate)
+  system.file("extdata", "depts.csv", package = "Cychecks")
+  expect_error(stats_mf(data = select(sals_dept, -total_salary_paid))) # no total_salary_paid column
   expect_error(stats_mf(data = select(sals_dept, -department))) # no department column
   expect_error(stats_mf(data = select(sals_dept, -gender))) # no gender column
   expect_error(stats_mf(data = select(sals_dept, -position))) # no position column
   expect_error(stats_mf(data = (sals_dept %>%
-                                  mutate(total_salary_paid = as.character(total_salary_paid))))) # salary isn't numeric
-
+                                  mutate(total_salary_paid = as.character(total_salary_paid)))))# salary isn't numeric
+  sals_test <- sal_df(fiscal_year = 2018, limit = 10000, token = "$$app_token=GJISvIEj4Jg2KmwRkV3oCGJrj") %>%
+    left_join(., depts, by = "name")
+  expect_equal(stats_mf(sals18) %>% select(p_val), stats_mf(sals_test) %>% select(p_val))
   }
   )
+
