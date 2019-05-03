ad4b4aa62992713692ae349e3f9afa199305586b
Khaledian <ykh@iastate.edu>
2019-05-03 08:54:45 -0500

    updating covr

--- tests/testthat/test_anon_fun.r
+++ tests/testthat/test_anon_fun.r
@@ -6,11 +6,11 @@ context("test_anonymize_function")
 test_that("Anonymize function is working", {
 
   df_exmple <- data.frame("name" = c("Brianna","Gina","Lydia","Stephanie","Yones"),
-                          "Salary" = c(5456, 5698, 5647, 5842, 5010))
+                          "Salary" = c(5456, 5698, 5647, 5842, 5910))
 
-  expect_error(anonymize(df = df_exmple, info = NULL)) # "df should be a data fram"etra
-  expect_error(anonymize(df = df_exmple, calss = NULL, cols_to_anon = "name")) #"to_anon should be a character vector"
-  expect_error(anonymize(df = df_exmple, regexp = NULL, algo = "crc32")) # algo is not a string
+  expect_error(anonymize(df = df_exmple, info = NULL)) # "df should be a data frame
+  expect_error(anonymize(df = df_exmple, calss = NULL, cols_to_anon = "name")) #"cols_to_anon should be a character vector"
+  expect_error(anonymize(df = df_exmple, regexp = NULL, algo = "crc32")) # algo is a string
 
   set.seed(30000)
   res <- anonymize(df = df_exmple)
